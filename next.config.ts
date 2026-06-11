82dee87a15cfa0cd6963df778e2adf051f0025d6
polat3361-hue <polat3361@gmail.com>
2026-06-11 22:41:27 +0300

    fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
 import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: [],
-  },
+  serverExternalPackages: ['cheerio'],
 };
 export default nextConfig;
